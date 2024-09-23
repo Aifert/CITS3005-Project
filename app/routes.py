6ec7086d75edf317968c62374c8a802c2ccc3812
Aifert Yet <aifertyet0628@gmail.com>
2024-09-23 15:39:43 +0800

    create knowledge graph and routes

--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,17 @@ from app import app, query_engine
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('search_form.html')
 
 @app.route('/search', methods=['POST'])
 def search():
     query = request.form['query']
-    results = query_engine.execute_query(query)
+    raw_results = query_engine.execute_query(query)
+
+    results = []
+
+    for row in raw_results:
+        uri_ref = row[0]
+        results.append(str(uri_ref))
+
     return render_template('results.html', results=results)
